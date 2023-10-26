5b00dd902647fdb51aeace00cf939d7a78948bc6
Ruslan Nazarov <rnazarov142@gmail.com>
2023-10-26 13:35:23 +0200

    Add project files.

---
+++ lab4_nazarov/Program.cs
@@ -0,0 +1,8 @@
+﻿//высокий уровень, 6
+Console.WriteLine("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+
+Console.WriteLine((x >= 0 && x <= 2 && y >= -2 && y <= 2) ? "Входит" : "Не входит");
